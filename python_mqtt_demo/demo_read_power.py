16e775063c31f9be641e2d0d12604c64e5476cdf
-- <meltbox360@gmail.com>
2021-12-12 17:50:14 -0500

    updated mqtt demos

--- python_mqtt_demo/demo_read_power.py
+++ python_mqtt_demo/demo_read_power.py
@@ -5,27 +5,26 @@ port = 1883
 client = mqttClient.Client("my_uniq_id_1936")
 connected = False
 
-MQTT_TOPICS_SUB = [("kauf-plug/sensor/kauf_plug_power/state",0),("LA/plug1/voltage",0)]
+MQTT_TOPICS_SUB = [("LA/plug1/power_meas",0),("LA/plug1/voltage",0)]
 
 def run_on_connect(client, userdata, flags, rc):
     if rc == 0:
-        conneected = True
+        global connected 
+        connected = True
         print("Connected...")
-        msg_val = '1';
-        topic = "LA/plug_1/power"
-        client.publish(topic, msg_val)
     else:
         print("Connection failed")
     
 def run_on_message(client, userdata, message):
     print("Got a message")
+    print(client)
     print("Wattage: " + str(message.payload))
     
 client.on_connect = run_on_connect
 client.on_message = run_on_message
 client.connect(broker, port)
 client.loop_start()
-while(~connected):
+while(not(connected)):
     time.sleep(0.1)
 
 client.subscribe(MQTT_TOPICS_SUB)
--- python_mqtt_demo/demo_switch_off.py
+++ python_mqtt_demo/demo_switch_off.py
@@ -3,18 +3,29 @@ import time
 broker = "192.168.1.75"
 port = 1883
 client = mqttClient.Client("my_uniq_id_1936")
+connected = False
+
 def run_on_connect(client, userdata, flags, rc):
     if rc == 0:
+        global connected
+        connected = True
+        print(connected)
         print("Connected...")
-        msg_val = '0';
-        topic = "LA/plug_1/power"
-        client.publish(topic, msg_val)
     else:
         print("Connection failed")
-    client.disconnect()
-    client.loop_stop()
     
 client.on_connect = run_on_connect
 client.connect(broker, port)
 client.loop_start()
-time.sleep(3);
+
+while(not(connected)):
+    print(connected)
+    time.sleep(0.1)
+
+msg_val = '0';
+topic = "LA/plug_1/power"
+client.publish(topic, msg_val)
+
+print("Exiting")
+client.disconnect()
+client.loop_stop()
