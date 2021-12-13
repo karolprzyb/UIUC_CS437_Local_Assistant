79e7b44a0c662dfc6db5a84b9176e2e611b50788
Karol Przybyszewski <karol.przyb@gmail.com>
2021-12-12 19:49:24 -0500

    updated plug to read data in example

--- python_mqtt_demo/demo_read_power.py
+++ python_mqtt_demo/demo_read_power.py
@@ -4,8 +4,9 @@ broker = "192.168.1.75"
 port = 1883
 client = mqttClient.Client("my_uniq_id_1936")
 connected = False
+waitingMessage = True
 
-MQTT_TOPICS_SUB = [("LA/plug1/power_meas",0),("LA/plug1/voltage",0)]
+MQTT_TOPICS_SUB = [("kauf-plug/sensor/kauf_plug_power/state",0),("kauf-plug/sensor/kauf_plug_current/state",0),("kauf-plug/sensor/kauf_plug_voltage/state",0), ("kauf-plug/sensor/kauf_plug_total_daily_energy/state",0)]
 
 def run_on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -17,21 +18,30 @@ def run_on_connect(client, userdata, flags, rc):
     
 def run_on_message(client, userdata, message):
     print("Got a message")
-    print(client)
+    print("Topic: " + str(message.topic))
     print("Wattage: " + str(message.payload))
+    print("=============================================================")
+    if str(message.topic) == "kauf-plug/sensor/kauf_plug_voltage/state":
+        global waitingMessage
+        waitingMessage = False
+        print('DONE')
     
 client.on_connect = run_on_connect
 client.on_message = run_on_message
+#client.on_publish = run_on_publish
+#client.on_subscribe = run_on_subscribe
 client.connect(broker, port)
-client.loop_start()
-while(not(connected)):
-    time.sleep(0.1)
-
-client.subscribe(MQTT_TOPICS_SUB)
 
 try:
-    while True:
-        time.sleep(1)
+    while waitingMessage:
+        print(waitingMessage)
+        client.loop_start()
+        while(not(connected)):
+            time.sleep(0.05)
+
+        #print(mqttClient.MQTT_ERR_SUCCESS)
+        print(client.subscribe(MQTT_TOPICS_SUB))
+        client.loop_stop();
 
 except KeyboardInterrupt:
     print ("exiting")
